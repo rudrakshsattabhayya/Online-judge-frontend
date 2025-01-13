9e30d8dffd0b5f8cb7b0c8b1ec92f6b8ef2816ee
Rudraksh Sattabhayya <rudrakshsattabhayya@gmail.com>
2025-01-13 19:33:44 +0530

    changing loader styles

--- src/Components/Loader/loader.js
+++ src/Components/Loader/loader.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./loaderStyles.css";
 
-const Loader = (props) => {
+const Loader = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 12000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
+    <div className="spinner-container">
       <div className="spinner">
         <div className="bounce1"></div>
         <div className="bounce2"></div>
         <div className="bounce3"></div>
       </div>
+      <div className="spinner-text">Loading...</div>
+      {showMessage && (
+        <div className="loader-message">
+          This may take up to a minute as the server is deployed on a free instance on render.com
+        </div>
+      )}
+    </div>
   );
 };
 
